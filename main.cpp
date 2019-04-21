fa7ec203a2a7190d83207e1f20a90a4f44988905
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2019-04-21 16:13:17 +0200

    move brightness value checking code

--- main.cpp
+++ main.cpp
@@ -59,12 +59,19 @@ int main(const int argc, const char** const argv)
 	auto brightness_opt = chartodouble(argv[2], argv[2] + std::strlen(argv[1]));
 
 	if(!brightness_opt.has_value()) {
-		std::cout << "Bad float brightness.\n";
+		std::cout << "Couldn't parse brightness value.\n";
 		print_usage({argc, argv});
 		return 2;
 	}
 
 	auto brightness = brightness_opt.value();
+
+	if(std::isnan(brightness) || std::isinf(brightness) || brightness < 0.0 || brightness > 100.0) {
+		std::cout << "Bad brightness value.\n";
+		print_usage({argc, argv});
+		return 5;
+	}
+
 
 	try {
 		const auto class_path = fs::path{"/sys/class/backlight"};
@@ -110,15 +117,6 @@ int get_max_brightness(const fs::path& base_path)
 
 void set_brightness(const double brightness, const fs::path& base_path)
 {
-	if(std::isnan(brightness))
-		throw std::out_of_range{"brightness brightness is NaN."};
-
-	if(std::isinf(brightness))
-		throw std::out_of_range{"brightness value is INF."};
-
-	if(brightness < 0.0 || brightness > 100.0)
-		throw std::out_of_range{"brightness value is out of range."};
-
 	const auto max = get_max_brightness(base_path);
 
 	const auto path = base_path / "brightness";
